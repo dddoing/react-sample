4e3114d72a21f7cace1cdc433f6d57f9ea075ac6
유도건 <dgyoo@nextree.io>
2021-11-16 00:22:50 +0900

    202111160022

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,7 @@
 import {useParams, useLocation} from 'react-router'
 import {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import {Route, Switch} from 'react-router-dom';
+import {Link,Route, Switch,useRouteMatch} from 'react-router-dom';
 import Price from './Price';
 import Chart from './Chart';
 
@@ -48,6 +48,28 @@ const OverviewItem = styled.div`
 `;
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
 `;
 
 interface Params {
@@ -123,9 +145,11 @@ function Coin() {
     const [coinDetail, setCoinDetail] = useState<CoinDetailInterface>({} as CoinDetailInterface);
     const [priceInfo, setPriceInfo] = useState<PriceInfoInterface>({} as PriceInfoInterface);
     const {state} = useLocation<RouteState>();
+    const priceMatch  = useRouteMatch("/:coinId/price");
+    const chartMatch = useRouteMatch("/:coinId/chart");
 
     // console.log(name)
-    const { coinId } = useParams<Params>();
+    const {coinId} = useParams<Params>();
     // console.log(coinId);
     useEffect(() => {
         //
@@ -144,7 +168,7 @@ function Coin() {
     return (
         <Container>
             <Header>
-                <Title>{state?.name || "Loading"}</Title>
+                <Title>{state?.name ? state.name : loading ? "Loading" : coinDetail?.name}</Title>
             </Header>
             {loading ? <Loader>"Loading"</Loader> : (
                 <>
@@ -173,12 +197,24 @@ function Coin() {
                             <span>{priceInfo?.max_supply}</span>
                         </OverviewItem>
                     </Overview>
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>
+                                Chart
+                            </Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>
+                                Price
+                            </Link>
+                        </Tab>
+                    </Tabs>
                     <Switch>
-                        <Route path={`/${coinId}/price`}>
-                            <Price />
+                        <Route path={`/:coinId/price`}>
+                            <Price/>
                         </Route>
-                        <Route path={`/${coinId}/chart`}>
-                            <Chart />
+                        <Route path={`/:coinId/chart`}>
+                            <Chart/>
                         </Route>
                     </Switch>
                 </>
